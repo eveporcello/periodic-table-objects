e4810bdd8ad2226c96bcee92a1ceb6e60ef06b2f
Eve Porcello <eve@moonhighway.com>
2021-02-17 21:27:12 +0000

    Step 2

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
-const instruments = [];
-// const instruments = new Array();
+const instruments = new Array();
+instruments[0] = "piano";
+instruments[1] = "guitar";
 
 console.log(instruments);
+console.log(instruments.length);
+console.log(instruments[1]);
+
+// const instruments = ["drums", "bass"];
